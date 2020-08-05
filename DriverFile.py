f80d903ccaa31b89681ed678a21086ad8b682da0
MicImbriani <imbr.mic@gmail.com>
2020-08-05 15:33:47 +0200

    Opens pics one by one in a folder

--- DriverFile.py
+++ DriverFile.py
@@ -5,6 +5,7 @@ import argparse
 import imutils
 import dlib
 import cv2
+import os
 
 
 
@@ -12,7 +13,8 @@ import cv2
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--shape-predictor", required=True, help="path to facial landmark predictor")
-ap.add_argument("-i", "--image", required=True, help="path to input image")
+#   ap.add_argument("-i", "--image", required=True, help="path to input image")
+ap.add_argument("-d","--directoryPath", required=True, help="path to directory with pictures")
 args = vars(ap.parse_args())
 
 
@@ -21,23 +23,26 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(args["shape_predictor"])
 faceAligner = FaceAligner(predictor, desiredFaceWidth=256)
 
-# load the input image, resize it, and convert it to grayscale
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=800)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# show the original input image and detect faces in the grayscale image
-cv2.imshow("Input", image)
-rects = detector(gray, 2)
 
-# loop over the face detections
-for rect in rects:
-    # extract the ROI of the *original* face, then align the face using facial landmarks
-    # using a counter ensures the # of the face is shown
-    counter = 1
-    (x, y, w, h) = rect_to_bb(rect)
-    faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
-    faceAligned = faceAligner.align(image, gray, rect)
-    # display the output images
-    cv2.imshow("Original", faceOrig)
-    cv2.imshow("Aligned", faceAligned)
-    cv2.waitKey(0)
+# extract the names of the pictures one by one to be parsed
+directory = args["directoryPath"]
+for picture in os.listdir(directory):
+    image = cv2.imread(os.path.join(directory, picture))
+    image = imutils.resize(image, width=800)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow("Input", image)
+    # detect faces in the grayscale image
+    rects = detector(gray, 2)
+
+    # loop over the face detections
+    for rect in rects:
+        # extract the ROI of the *original* face, then align the face using facial landmarks
+        # using a counter ensures the # of the face is shown
+        counter = 1
+        (x, y, w, h) = rect_to_bb(rect)
+        #faceOrig = imutils.resize(image[y:y + h, x:x + w], width=256)
+        faceAligned = faceAligner.align(image, gray, rect)
+        # display the output images
+        #cv2.imshow("Original", faceOrig)
+        cv2.imshow("Output", faceAligned)
+        cv2.waitKey(10)
